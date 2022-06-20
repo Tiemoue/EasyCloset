e070c42c8fdcf8f6b5b4f7eae708dc9f1a88fd0e
Collins Ikpeyi <Ikpeyi89@gmail.com>
2022-06-20 14:31:31 -0700

    Added textview and buttons to signUpActivity

--- app/src/main/java/com/example/easycloset/SignUpActivity.java
+++ app/src/main/java/com/example/easycloset/SignUpActivity.java
@@ -3,12 +3,40 @@ package com.example.easycloset;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    Spinner spinner;
+    String genderChoice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        spinner = findViewById(R.id.spGenders);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.genders, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                genderChoice = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
     }
 }

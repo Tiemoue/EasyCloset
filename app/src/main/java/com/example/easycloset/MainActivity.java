579636365afcafaa1fdb2e1ec220723993f7b3ad
Collins Ikpeyi <Ikpeyi89@gmail.com>
2022-06-20 16:44:57 -0700

    Added Bottom navigation and toolbar to main activity

--- app/src/main/java/com/example/easycloset/MainActivity.java
+++ app/src/main/java/com/example/easycloset/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.easycloset;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar =findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()== R.id.miLogout){
+            logOutUser();
+        }return true;
+    }
+
+    private void logOutUser() {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }

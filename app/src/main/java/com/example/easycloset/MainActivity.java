2a3b92d862f178116ccdb368dce9deaaf9eccdcc
Collins Ikpeyi <Ikpeyi89@gmail.com>
2022-06-20 17:32:51 -0700

    Created Home, closet and profile fragments

--- app/src/main/java/com/example/easycloset/MainActivity.java
+++ app/src/main/java/com/example/easycloset/MainActivity.java
@@ -3,14 +3,24 @@ package com.example.easycloset;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
+    public BottomNavigationView bottomNavigationView;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    public MainFragment mainFragment = new MainFragment();
+    public ClosetFragment closetFragment = new ClosetFragment();
+    public ProfileFragment profileFragment = new ProfileFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,36 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar =findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+
+        bottomNavigationView.setOnItemSelectedListener(
+                new NavigationBarView.OnItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment fragment = null;
+                        switch (item.getItemId()) {
+                            case R.id.action_main:
+                                fragment = mainFragment;
+                                break;
+                            case R.id.action_closet:
+                                fragment = closetFragment;
+                                break;
+                            default:
+                                fragment = profileFragment;
+                                break;
+                        }
+                        fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                        return true;
+                    }
+                });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_main);
+
+
+
+
     }
 
 
--- app/src/main/java/com/example/easycloset/SignUpActivity.java
+++ app/src/main/java/com/example/easycloset/SignUpActivity.java
@@ -9,7 +9,6 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 

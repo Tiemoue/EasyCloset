e7cb831ecf8fe52b84a44a127628e3f34cb74f0f
Collins Ikpeyi <Ikpeyi89@gmail.com>
2022-06-22 11:13:11 -0700

    created instance of weather , textview , edittext and imageview in the homefragment

--- app/src/main/java/com/example/easycloset/HomeFragment.java
+++ app/src/main/java/com/example/easycloset/HomeFragment.java
@@ -3,26 +3,62 @@ package com.example.easycloset;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.RequestParams;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.Headers;
 
 
 public class HomeFragment extends Fragment {
+
+    Weather weather;
+    ImageView search;
+    EditText etCity;
+    TextView tvCity, tvCountry, tvTemp, tvForecast, tvHumidity, tvMinTemp, tvMaxTemp, tvSunrise, tvSunset;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        search = view.findViewById(R.id.search);
+        etCity = view.findViewById(R.id.etYourCity);
+        tvCity = view.findViewById(R.id.tvCity);
+        tvCountry = view.findViewById(R.id.tvCountry);
+        tvTemp = view.findViewById(R.id.tvTemp);
+        tvForecast = view.findViewById(R.id.tvForecast);
+        tvHumidity = view.findViewById(R.id.tvHumidity);
+        tvMinTemp = view.findViewById(R.id.tvMinTemp);
+        tvMaxTemp = view.findViewById(R.id.tvMaxTemp);
+        tvSunrise = view.findViewById(R.id.tvSunrises);
+        tvSunset = view.findViewById(R.id.tvSunsets);
+
+
     }
 }

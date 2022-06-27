04eb9e428e1adb5f7e3b82b04712132774905644
Collins Ikpeyi <Ikpeyi89@gmail.com>
2022-06-27 14:57:10 -0700

    Created Button instances

--- app/src/main/java/com/example/easycloset/CameraActivity.java
+++ app/src/main/java/com/example/easycloset/CameraActivity.java
@@ -3,12 +3,23 @@ package com.example.easycloset;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class CameraActivity extends AppCompatActivity {
+
+    private Button btnTakePicture;
+    private EditText etColour;
+    private Button btnUpload;
+    private ImageView ivPicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        btnTakePicture = findViewById(R.id.btnTakePicture);
+        btnUpload = findViewById(R.id.btnUpload);
     }
 }

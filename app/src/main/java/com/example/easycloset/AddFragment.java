cee5d8afae09b6ab793d6183bd88617768df268e
Collins Ikpeyi <Ikpeyi89@gmail.com>
2022-06-28 08:25:08 -0700

    Created AddFragment,UploadFragment, fragment_add.xml, and fragment_upload.xml

---
+++ app/src/main/java/com/example/easycloset/AddFragment.java
@@ -0,0 +1,55 @@
+package com.example.easycloset;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class AddFragment extends Fragment {
+
+    private Button btnCoats;
+    private Button btnJackets;
+    private Button btnSweaters;
+    private Button btnShirts;
+    private Button btnHoodies;
+    private Button btnShorts;
+    private Button btnPants;
+    private Button btnJoggers;
+    private Button btnBoots;
+    private Button btnSneakers;
+    private Button btnSlides;
+    private Button btnHeadwear;
+    private Button btnDone;
+    private MainActivity activity;
+
+    public AddFragment() {
+    }
+
+    public AddFragment(MainActivity mainActivity) {
+        activity = mainActivity;
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        btnDone = view.findViewById(R.id.btDone);
+        btnCoats = view.findViewById(R.id.btCoat);
+
+        btnCoats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activity.fragmentManager.beginTransaction().replace(R.id.flContainer, activity.getUploadFragment()).commit();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/easycloset/UploadFragment.java
+++ app/src/main/java/com/example/easycloset/UploadFragment.java
@@ -1,29 +1,36 @@
 package com.example.easycloset;
 
-import static android.content.ContentValues.TAG;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
+import static android.app.Activity.RESULT_OK;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
+
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.parse.ParseUser;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.SaveCallback;
 
 import java.io.File;
 
-public class CameraActivity extends AppCompatActivity {
+
+public class UploadFragment extends Fragment {
 
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 20;
     public static String TAG = ".CameraActivity";
@@ -33,17 +40,27 @@ public class CameraActivity extends AppCompatActivity {
     private ImageView ivPicture;
     private File photoFile;
     public String photoFileName = "photo.jpg";
+    private MainActivity activity;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera);
+    public UploadFragment() {
+    }
 
-        btnTakePicture = findViewById(R.id.btnTakePicture);
-        btnUpload = findViewById(R.id.btnUpload);
-        etColour = findViewById(R.id.etColour);
-        ivPicture = findViewById(R.id.ivPicture);
-        
+    public UploadFragment(MainActivity mainActivity) {
+        activity = mainActivity;
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_upload, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        btnTakePicture = view.findViewById(R.id.btnTakePicture);
+        btnUpload = view.findViewById(R.id.btnUpload);
+        etColour = view.findViewById(R.id.etColour);
+        ivPicture = view.findViewById(R.id.ivPicture);
+
         btnTakePicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,16 +71,15 @@ public class CameraActivity extends AppCompatActivity {
         btnUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String description = etColour.getText().toString();
-                if (description.isEmpty()){
-                    Toast.makeText(CameraActivity.this, "Colour cannot be empty", Toast.LENGTH_SHORT).show();
+                String colour = etColour.getText().toString();
+                if (colour.isEmpty()) {
+                    Toast.makeText(getContext(), "Colour cannot be empty", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if(photoFile != null && ivPicture.getDrawable() != null){
-
-                }
-                else{
-                    Toast.makeText(CameraActivity.this, "no image found", Toast.LENGTH_SHORT).show();
+                if (photoFile != null && ivPicture.getDrawable() != null) {
+                    saveItem(colour, photoFile);
+                } else {
+                    Toast.makeText(getContext(), "no image found", Toast.LENGTH_SHORT).show();
                     return;
                 }
             }
@@ -75,11 +91,11 @@ public class CameraActivity extends AppCompatActivity {
         // Create a File reference for future access
         photoFile = getPhotoFileUri(photoFileName);
         // wrap File object into a content provider
-        Uri fileProvider = FileProvider.getUriForFile(this, "com.codepath.fileprovider", photoFile);
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider", photoFile);
         intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
         // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
         // So as long as the result is not null, it's safe to use the intent.
-        if (intent.resolveActivity(this.getPackageManager()) != null) {
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
             // Start the image capture intent to take photo
             startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
         }
@@ -96,7 +112,7 @@ public class CameraActivity extends AppCompatActivity {
                 // Load the taken image into a preview
                 ivPicture.setImageBitmap(takenImage);
             } else { // Result was a failure
-                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
             }
         }
     }
@@ -105,12 +121,33 @@ public class CameraActivity extends AppCompatActivity {
         // Get safe storage directory for photos
         // Use `getExternalFilesDir` on Context to access package-specific directories.
         // This way, we don't need to request external read/write runtime permissions.
-        File mediaStorageDir = new File(this.getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
         // Create the storage directory if it does not exist
-        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
         }
         // Return the file target for the photo based on filename
         File file = new File(mediaStorageDir.getPath() + File.separator + photoFileName);
         return file;
+    }
+
+    private void saveItem(String colour, File photoFile) {
+        Item item = new Item();
+        item.setColour(colour);
+        item.setImage(new ParseFile(photoFile));
+        item.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Toast.makeText(getContext(), "Error while saving", Toast.LENGTH_SHORT).show();
+                } else {
+                    etColour.setText("");
+                    ivPicture.setImageResource(0);
+                    Toast.makeText(getContext(), "Sent", Toast.LENGTH_SHORT).show();
+                    activity.getClosetFragment().allItems.add(0, item);
+                    activity.getClosetFragment().adapter.notifyDataSetChanged();
+                    activity.fragmentManager.beginTransaction().replace(R.id.flContainer, activity.getClosetFragment()).commit();
+                }
+            }
+        });
     }
 }

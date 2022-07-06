4c7d6008ca15d8a970b00a7380441210247a805e
Collins Ikpeyi <Ikpeyi89@gmail.com>
2022-07-06 13:43:29 -0700

    addressed feedback

--- app/src/main/java/com/example/easycloset/ClosetFragment.java
+++ app/src/main/java/com/example/easycloset/ClosetFragment.java
@@ -3,11 +3,21 @@ package com.example.easycloset;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 
 public class ClosetFragment extends Fragment {
@@ -22,4 +32,7 @@ public class ClosetFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_closet, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    }
 }
--- app/src/main/java/com/example/easycloset/Item.java
+++ app/src/main/java/com/example/easycloset/Item.java
@@ -9,7 +9,9 @@ import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 @ParseClassName("Item")
 public class Item extends ParseObject {
@@ -52,38 +54,7 @@ public class Item extends ParseObject {
     }
 
     @NonNull
-    public static String calculateTimeAgo(Date createdAt) {
-
-        int SECOND_MILLIS = 1000;
-        int MINUTE_MILLIS = 60 * SECOND_MILLIS;
-        int HOUR_MILLIS = 60 * MINUTE_MILLIS;
-        int DAY_MILLIS = 24 * HOUR_MILLIS;
-
-        try {
-            createdAt.getTime();
-            long time = createdAt.getTime();
-            long now = System.currentTimeMillis();
-
-            final long diff = now - time;
-            if (diff < MINUTE_MILLIS) {
-                return "just now";
-            } else if (diff < 2 * MINUTE_MILLIS) {
-                return "a minute ago";
-            } else if (diff < 50 * MINUTE_MILLIS) {
-                return diff / MINUTE_MILLIS + " m";
-            } else if (diff < 90 * MINUTE_MILLIS) {
-                return "an hour ago";
-            } else if (diff < 24 * HOUR_MILLIS) {
-                return diff / HOUR_MILLIS + " h";
-            } else if (diff < 48 * HOUR_MILLIS) {
-                return "yesterday";
-            } else {
-                return diff / DAY_MILLIS + " d";
-            }
-        } catch (Exception e) {
-            Log.i("Error:", "getRelativeTimeAgo failed", e);
-            e.printStackTrace();
-        }
-        return "";
+    public static String Time(Date createdAt) {
+        return new SimpleDateFormat("EEE, MMM d, ''yy", Locale.ENGLISH).format(new Date(String.valueOf(createdAt)));
     }
 }

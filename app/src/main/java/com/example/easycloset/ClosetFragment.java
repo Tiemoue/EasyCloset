4b593f1afbf1458f28a6c08be0f88e14b960d423
Collins Ikpeyi <Ikpeyi89@gmail.com>
2022-07-11 09:07:19 -0700

    addressed feedback

--- app/src/main/java/com/example/easycloset/ClosetFragment.java
+++ app/src/main/java/com/example/easycloset/ClosetFragment.java
@@ -4,18 +4,15 @@ package com.example.easycloset;
 import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.parse.ParseQuery;
 
@@ -30,12 +27,6 @@ public class ClosetFragment extends Fragment {
     private ProgressDialog progressDialog;
 
     public ClosetFragment() {
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        adapter.notifyDataSetChanged();
     }
 
     public ClosetFragment(MainActivity mainActivity) {

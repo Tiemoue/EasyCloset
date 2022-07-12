1cdfb53b44ab5faaa0b941093d4c1131f40b445e
Collins Ikpeyi <Ikpeyi89@gmail.com>
2022-07-12 10:13:11 -0700

    Added ItemDetail xml

--- app/src/main/java/com/example/easycloset/ItemDetailActivity.java
+++ app/src/main/java/com/example/easycloset/ItemDetailActivity.java
@@ -1,4 +1,54 @@
 package com.example.easycloset;
 
-public class ItemDetailActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.parse.DeleteCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+
+import java.util.Date;
+
+public class ItemDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_detail);
+
+        ImageView ivItem = findViewById(R.id.ivItem);
+        TextView tvCategory = findViewById(R.id.tvItemCategory);
+        TextView tvColour = findViewById(R.id.tvItemColor);
+        TextView tvDate = findViewById(R.id.tvDateAdded);
+        ImageButton btnDelete = findViewById(R.id.ibDelete);
+
+        Item item = getIntent().getParcelableExtra(Item.class.getSimpleName());
+
+        Date createdAt = item.getCreatedAt();
+        String time = Item.calculateTimeAgo(createdAt);
+
+        tvCategory.setText(item.getCategory());
+        tvColour.setText(item.getColour());
+        tvDate.setText(time);
+        ParseFile image = item.getImage();
+        Glide.with(this).load(image.getUrl()).into(ivItem);
+
+        btnDelete.setOnClickListener(v -> {
+            item.deleteInBackground(new DeleteCallback() {
+                @Override
+                public void done(ParseException e) {
+                    Intent intent = new Intent(ItemDetailActivity.this, MainActivity.class);
+                    intent.putExtra(Item.class.getSimpleName(), item);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
+            });
+        });
+    }
 }

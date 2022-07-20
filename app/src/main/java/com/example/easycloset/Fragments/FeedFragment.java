427f1af7c04e3462e9de9fcf5bf8c989db4e95f8
Collins Ikpeyi <Ikpeyi89@gmail.com>
2022-07-20 14:59:40 -0700

    created the feed fragment and xml

---
+++ app/src/main/java/com/example/easycloset/Fragments/FeedFragment.java
@@ -0,0 +1,31 @@
+package com.example.easycloset.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.easycloset.R;
+
+public class FeedFragment extends Fragment {
+
+    public FeedFragment() {
+        // Required empty public constructor
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_feed, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
